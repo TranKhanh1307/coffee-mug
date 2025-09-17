4a62999979ffd60cc597de921c6db4f69fac73b7
TranKhanh1307 <trankhanh7031@gmail.com>
2025-09-17 12:59:53 +0700

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
 export default function Banner() {
   return (
-    <div className="h-[530px] w-full bg-[url('https://assets.website-files.com/5be96251aaba7a7b19ecdf69/5be96251aaba7a58aaecdfba_Header-Pic.jpg')] bg-cover"></div>
+    <div className="m-auto h-[530px] max-w-7xl bg-[url('https://assets.website-files.com/5be96251aaba7a7b19ecdf69/5be96251aaba7a58aaecdfba_Header-Pic.jpg')] bg-cover bg-bottom"></div>
   );
 }
